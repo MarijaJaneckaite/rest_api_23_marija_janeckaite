1d68c1127141928a7dc30bb5218bcd021ce8467e
MarijaJaneckaite <marija.janeckaite@gmail.com>
2023-05-03 12:25:41 +0300

    Add folder structure and dependencies

---
+++ src/test/java/rest_api_23_marija_janeckaite/constants/ProjectConstants.java
@@ -0,0 +1,6 @@
+package rest_api_23_marija_janeckaite.constants;
+
+public class ProjectConstants {
+    public static final String API_TOKEN = "pk_54597699_HZ0R0L9A19ZY21HBUP3APM7KSCURSEVO";
+    public static final String SPACE_ID = "90040203376";
+}
