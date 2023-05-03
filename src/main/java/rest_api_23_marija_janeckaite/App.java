95be08fb3d026ddd6287c5009dcd616ce5d66f3b
MarijaJaneckaite <marija.janeckaite@gmail.com>
2023-05-03 11:29:47 +0300

    Initial commit

---
+++ src/main/java/rest_api_23_marija_janeckaite/App.java
@@ -0,0 +1,13 @@
+package rest_api_23_marija_janeckaite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
